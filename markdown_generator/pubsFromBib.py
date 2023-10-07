4c5d670b6b8bdd879fe7a68655cdb971dda4b225
Jurlind Budurushi <jurlind.budurushi@cloudical.io>
2023-10-07 22:55:02 +0300

    work in progress

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -27,20 +27,28 @@ import re
 #todo: incorporate different collection types rather than a catch all publications, requires other changes to template
 publist = {
     "proceeding": {
-        "file" : "proceedings.bib",
+        "file": "editor.bib",
         "venuekey": "booktitle",
         "venue-pretext": "In the proceedings of ",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
+        "collection": {"name":"proceeding",
+                        "permalink":"/proceedings/"}
         
-    },
-    "journal":{
-        "file": "pubs.bib",
-        "venuekey" : "journal",
-        "venue-pretext" : "",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-    } 
+    }
+   # "journal":{
+        #"file": "journals.bib",
+       # "venuekey": "journal",
+      #  "venue-pretext" : "",
+     #   "collection": {"name":"journal",
+    #                    "permalink":"/journals/"}
+   # },
+
+    #"editor":{
+        #"file": "editor.bib",
+        #"venuekey": "journal",
+       # "venue-pretext" : "",
+      #  "collection": {"name":"editor",
+     #                   "permalink":"/editor/"}
+    #}
 }
 
 html_escape_table = {
